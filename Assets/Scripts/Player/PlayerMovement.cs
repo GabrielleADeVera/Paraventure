be9a74d43f934e698a716b2b24d3e192db2aaa2e
Gabrielle <gabbydvr@hotmail.com>
2018-03-23 22:17:49 -0700

    Added jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,33 +2,35 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float speed = 6f;            // The speed that the player will move at.
-
+	public float speed = 2f;  // The speed that the player will move at.
+	public float maxJumpHeight = 6f;
+	public float jumpVelocity = 6f;
 	Vector3 movement;                   // The vector to store the direction of the player's movement.
-//	Animator anim;                      // Reference to the animator component.
 	Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
-//	int floorMask;                      // A layer mask so that a ray can be cast just at gameobjects on the floor layer.
-//	float camRayLength = 100f;          // The length of the ray from the camera into the scene.
 
 	void Awake ()
 	{
-		// Create a layer mask for the floor layer.
-//		floorMask = LayerMask.GetMask ("Floor");
-
-		// Set up references.
-//		anim = GetComponent <Animator> ();
 		playerRigidbody = GetComponent <Rigidbody> ();
 	}
 
 
-	void FixedUpdate ()
+	void Update ()
 	{
 		// Store the input axes.
-		float h = Input.GetAxisRaw ("Horizontal");
-		float v = Input.GetAxisRaw ("Vertical");
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+//		float y = transform.position.y;
 
-		// Move the player around the scene.
-		Move (h, v);
+
+		if (Input.GetKeyDown ("space")) {
+			print ("space key was pressed");
+			playerRigidbody.velocity = Vector3.up * jumpVelocity;
+		
+		}
+
+
+		Move (moveHorizontal, 0f, moveVertical);
+
 
 
 
@@ -39,10 +41,10 @@ public class PlayerMovement : MonoBehaviour
 //		Animating (h, v);
 	}
 
-	void Move (float h, float v)
+	void Move (float x, float y, float z)
 	{
 		// Set the movement vector based on the axis input.
-		movement.Set (h, 0f, v);
+		movement.Set (x, y, z);
 
 		// Normalise the movement vector and make it proportional to the speed per second.
 		movement = movement.normalized * speed * Time.deltaTime;
