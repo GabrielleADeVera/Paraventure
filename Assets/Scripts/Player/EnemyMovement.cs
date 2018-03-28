b0277626beef2ebcb546482ef72738b1de4c87ed
Gabrielle <gabbydvr@hotmail.com>
2018-03-28 12:35:30 -0700

    Added some Enemy Script

---
+++ Assets/Scripts/Player/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform player;
+	PlayerHealth playerHealth;
+//	EnemyHealth enemyHealth;
+//	NavMeshAgent nav;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerHealth = player.GetComponent <PlayerHealth> ();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerHealth.currentHealth <= 0)
+			return;
+		
+	}
+}
