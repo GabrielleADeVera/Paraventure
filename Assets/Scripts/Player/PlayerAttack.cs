1f31620f4f502f667742894f3df79d8b294e927b
Gabrielle <gabbydvr@hotmail.com>
2018-04-04 19:32:42 -0700

    Added Attack to LMB, Attack Cooldown

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerAttack : MonoBehaviour {
 
 	public SphereCollider sphereCollider;
+	public float AttackCooldown = 2f;
+	public float NextAttack;
 
 
 	// Use this for initialization
@@ -12,17 +15,17 @@ public class PlayerAttack : MonoBehaviour {
 		sphereCollider = GetComponent <SphereCollider>();
 		
 	}
-	void OnTriggerEnter(Collider other){
-		if(other.gameObject.CompareTag("Enemy")){
-			
+	void OnTriggerStay(Collider other){
+		if(other.gameObject.CompareTag("Enemy") && Input.GetMouseButtonDown(0) && Time.time > NextAttack){
+			NextAttack = Time.time + AttackCooldown;
 			EnemyHealth enemyHealth = other.GetComponent<EnemyHealth> ();
 			enemyHealth.currentHealth -= 7;
-			Debug.Log ("Enemy Health is " + enemyHealth.currentHealth);
+				Debug.Log ("Enemy Health is " + enemyHealth.currentHealth);
 
 			}
 	}
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
