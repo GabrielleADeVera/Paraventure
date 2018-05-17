bc6fcbbc05a907793d819cb815b0e0588e22a063
Gabrielle <gabbydvr@hotmail.com>
2018-05-17 16:44:52 -0700

    Menu Screen

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -6,15 +6,19 @@ public class ApplicationManager : MonoBehaviour {
 
 	public string scene;
 	private int load;
-	void Start ()
+
+
+
+	void Start (){
+	}
 	
 
 	public void Newgame ()
 	{
 		Debug.Log (scene);
 
-		//SceneManager.GetSceneByName (scene) = load;
-		//SceneManager.LoadScene (load);
+//		SceneManager.GetSceneByName (scene) = load;
+		SceneManager.LoadScene (1);
 		
 	}//public GameObject.Scene scene;
 
